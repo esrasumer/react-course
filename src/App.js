18fc03bf1b00f2f08bd74a6a077af72689442f96
esrasumer <esrasumer28@gmail.com>
2022-10-26 14:42:43 +0300

    deleted file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Footerr from './components/Footerr';
 import Headerr from './components/Headerr';
-import Content from './Content';
-import Employees from './Employees';
-import Footer from './Footer';
-import Header from './Header';
+
 
 function App() {
 
@@ -17,11 +14,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Header />
-      <Content />
-      <Employees />
-      <Footer /> */}
-
       <Headerr theme={theme}/>
       <Footerr theme={theme} setTheme={setTheme}/>
 
--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Footer() {
-  return (
-    <div  className='mt-4 p5 bg-danger text--white rounded'>Footer</div>
-  )
-}
-
-export default Footer;
